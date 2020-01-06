1d49afc15165012679874a56f0aaca70f91433c9
Emily <54571022+naneko@users.noreply.github.com>
2020-01-06 13:10:00 -0500

    Update umaru.py

--- umaru.py
+++ umaru.py
@@ -4,6 +4,7 @@ import discord
 
 TOKEN = os.environ['DISCORD_TOKEN']
 
+umarucry = '<:umarucry:615726271212552203>'
 
 class MyClient(discord.Client):
     async def on_message(self, message):
@@ -13,7 +14,7 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
-        if message.content not in '<:umarucry:663799760964157451>' and "Umaru Moderator" not in roles and message.channel.name in channels:
+        if message.content not in umarucry and "Umaru Moderator" not in roles and message.channel.name in channels:
             print(message.created_at)
             print(message.guild)
             print(message.channel.name)
@@ -22,19 +23,19 @@ class MyClient(discord.Client):
             try:
                 await message.delete()
             except Exception as error:
-                await message.channel.send('\:umarucry: Server owner need to give me the `Manage Messages` permission!')
-            await message.channel.send('\:umarucry: {0.author.mention}'.format(message))
+                await message.channel.send(f'{umarucry} Server owner need to give me the `Manage Messages` permission!')
+            await message.channel.send('{} {0.author.mention}'.format(umarucry, message))
             print("---")
 
-        elif client.user in message.mentions:
-            emoji = client.get_emoji(663799760964157451)
+#         elif client.user in message.mentions:
+#             emoji = client.get_emoji(663799760964157451)
 #             try:
 #                 await message.add_reaction(emoji)
 #             except Exception as error:
 #                 await message.channel.send('<:umarucry:663799760964157451> Server owner need to give me the `Add Reactions` oermission!')
 
-        elif message.content not in '<:umarucry:663799760964157451>':
-            emoji = client.get_emoji(663799760964157451)
+#         elif message.content not in '<:umarucry:663799760964157451>':
+#             emoji = client.get_emoji(663799760964157451)
 #             try:
 #                 await message.add_reaction(emoji)
 #             except Exception as error:
